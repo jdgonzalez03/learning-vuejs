2a42429b98a7718c24813b8a47fe381ec134af9f
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-09-12 22:26:56 -0500

    Add progress step

---
+++ step-progress/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import StepProgress from './components/StepProgress.vue';
+</script>
+
+<template>
+  <StepProgress />
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: 'Arial', sans-serif;
+}
+
+body {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
